ad8045c1582fa092b38f5c4a70230cf3aa38e4b7
LocDev <nguyenvanlocmmo@gmail.com>
2025-03-17 10:55:19 +0700

    new stack locDev

--- src/main/java/org/example/matrix/_2643_Row_With_Maximum_Ones.java
+++ src/main/java/org/example/matrix/_2643_Row_With_Maximum_Ones.java
@@ -1,4 +1,42 @@
 package org.example.matrix;
 
 public class _2643_Row_With_Maximum_Ones {
+
+    public static int[] rowAndMaximumOnes(int[][] mat) {
+        int[] result = new int[2];
+        int currentMax = 0;
+        for (int i = 0; i < mat.length; i++) {
+            int temp = 0;
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] == 1) temp++;
+            }
+            if (temp > currentMax) {
+                currentMax = temp;
+                result[0] = i;
+                result[1] = currentMax;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = {{0,1},{1,0}};
+        System.out.println(rowAndMaximumOnes(mat));
+    }
+
+//    Example 1:
+//
+//    Input: mat = [[0,1],[1,0]]
+//    Output: [0,1]
+//    Explanation: Both rows have the same number of 1's. So we return the index of the smaller row, 0, and the maximum count of ones (1). So, the answer is [0,1].
+//    Example 2:
+//
+//    Input: mat = [[0,0,0],[0,1,1]]
+//    Output: [1,2]
+//    Explanation: The row indexed 1 has the maximum count of ones (2). So we return its index, 1, and the count. So, the answer is [1,2].
+//    Example 3:
+//
+//    Input: mat = [[0,0],[1,1],[0,0]]
+//    Output: [1,2]
+//    Explanation: The row indexed 1 has the maximum count of ones (2). So the answer is [1,2].
 }
--- src/main/java/org/example/stack/_20_Valid_Parentheses.java
+++ src/main/java/org/example/stack/_20_Valid_Parentheses.java
@@ -1,4 +1,51 @@
 package org.example.stack;
 
+import java.util.Stack;
+
 public class _20_Valid_Parentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (isCouple(stack.peek(), c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isCouple(char inStack, char current) {
+        return inStack == '(' && current == ')'
+                || inStack == '{' && current == '}'
+                || inStack == '[' && current == ']';
+    }
+
+    public static void main(String[] args) {
+        String s = "(])";
+        System.out.println(isValid(s));
+    }
+//    Example 1:
+//    Input: s = "()"
+//    Output: true
+
+//    Example 2:
+//    Input: s = "()[]{}"
+//    Output: true
+//
+//    Example 3:
+//    Input: s = "(]"
+//    Output: false
+//
+//    Example 4:
+//    Input: s = "([])"
+//    Output: true
 }
